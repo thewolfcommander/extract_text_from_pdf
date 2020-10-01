fcf66469998bd86fe135aa1a0c37cb9da5bfa6cc
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-10-01 18:30:46 +0530

    Completed text extraction from PDF File

---
+++ app2.py
@@ -0,0 +1,33 @@
+
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+
+def convert_pdf_to_string(file_path):
+    output_string = StringIO()
+
+    with open(file_path, 'rb') as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
+
+    return output_string.getvalue()
+
+
+def driver():
+    data = convert_pdf_to_string('example2.pdf')
+    res = " ".join(data.split()) 
+    return res
+
+if __name__ == '__main__':
+    print(driver())
---
+++ update.sh
@@ -0,0 +1,4 @@
+git remote add origin git@github.com:thewolfcommander/extract_text_from_pdf.git
+git add .
+git commit -m "Completed text extraction from PDF File"
+git push origin master
